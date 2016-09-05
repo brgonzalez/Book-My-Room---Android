98533c08fe122ada2ebdb4091c726553b72fc3f7
Brayan Gonzalez Chavez <bgonzalez@snap.snaptechnology.net>
2016-09-05 17:33:41 -0600

    Update project

--- server/src/main/java/com/snaptechnology/bgonzalez/socket/Device.java
+++ server/src/main/java/com/snaptechnology/bgonzalez/socket/Device.java
@@ -4,4 +4,22 @@ package com.snaptechnology.bgonzalez.socket;
  * Created by bgonzalez on 05/09/2016.
  */
 public class Device {
+    private String ip;
+    private String macAddress;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
 }
--- server/src/main/java/com/snaptechnology/bgonzalez/socket/SenderSocket.java
+++ server/src/main/java/com/snaptechnology/bgonzalez/socket/SenderSocket.java
@@ -6,34 +6,42 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
 
 /**
  * Created by bgonzalez on 05/09/2016.
  */
 public class SenderSocket {
 
-    private static final int SERVERPORT = 6000;
-
-    private static final String SERVER_IP = "10.110.10.149";
-
-
+    private static final int RECEIVERPORT = 6000;
+    
+    
     private static Socket socket;
 
+    private List<Device> devices ;
+
+
+    public void sendData(String data){
+        InetAddress deviceAddress = null;
+        PrintWriter printer ;
+
+        for(Device device : devices){
+            try {
+                deviceAddress = InetAddress.getByName(device.getIp());
+                socket = new Socket(deviceAddress,RECEIVERPORT);
+                printer = new PrintWriter(new BufferedWriter( new OutputStreamWriter(socket.getOutputStream())), true);
+                printer.println(data);
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String args[]) throws IOException {
-
-        String str = "Test sending data from server";
-
-        InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-
-        socket = new Socket(serverAddr, SERVERPORT);
-
-
-
-        PrintWriter out = new PrintWriter(new BufferedWriter( new OutputStreamWriter(socket.getOutputStream())),
-                true);
-
-        out.println(str);
-
 
     }
 }
