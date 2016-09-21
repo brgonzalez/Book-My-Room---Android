463012db1159eee32f0fdaa78c27b21d84085172
bgonzalez <brgonzalez@itcr.ac.cr>
2016-09-21 16:42:41 -0600

    Adding class status to send status code to devices

---
+++ server/src/main/java/com/snaptechnology/bgonzalez/model/Status.java
@@ -0,0 +1,21 @@
+package com.snaptechnology.bgonzalez.model;
+
+/**
+ * Created by bgonzalez on 21/09/2016.
+ */
+public class Status {
+
+    private int statusCode;
+
+    public Status(){
+
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
