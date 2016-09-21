cd4565790be1e9fe791bf0dcb44a3b6e5e8fd420
bgonzalez <brgonzalez@itcr.ac.cr>
2016-09-21 16:44:38 -0600

    Getting locations from rest controller

--- server/src/main/java/com/snaptechnology/bgonzalez/controllers/LocationController.java
+++ server/src/main/java/com/snaptechnology/bgonzalez/controllers/LocationController.java
@@ -1,12 +1,15 @@
 package com.snaptechnology.bgonzalez.controllers;
 
 import com.snaptechnology.bgonzalez.JPA.repository.LocationRepository;
+import com.snaptechnology.bgonzalez.model.Location;
 import com.snaptechnology.bgonzalez.services.LocationService;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * LocationController is a controller to manipulate request of location,
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RequestMapping("/location")
 public class LocationController {
     @Autowired
     LocationRepository repository;
@@ -24,14 +28,15 @@ public class LocationController {
     @Autowired
     LocationService locationService ;
 
-    @RequestMapping("/location/findall")
-    public String findAll(){
+    @RequestMapping("/findall")
+    public
+    List<Location> findAll(){
         JSONArray jsonArray = new JSONArray();
 
         locationService.getAllLocations().forEach(jsonArray::put);
         JSONObject jsonObject = new JSONObject();
 
         jsonObject.put("locations", jsonArray);
-        return jsonObject.toString() ;
+        return locationService.getAllLocations() ;
     }
 }
